33e9be1fb34c1ae1ad55fe5f53e46190b652e779
Wale Akinbolagbe <aakinbolagbe@gmail.com>
2023-02-16 18:45:12 -0800

    added tommy loader and made changes

--- code/stage_3_code/Dataset_Loader.py
+++ code/stage_3_code/Dataset_Loader.py
@@ -8,6 +8,7 @@ Concrete IO class for a specific dataset
 from code.base_class.dataset import dataset
 from code.base_class.setting import setting
 import numpy as np
+import pickle
 import csv
 
 
@@ -15,33 +16,59 @@ class Dataset_Loader(dataset, setting):
     data = None
     dataset_source_folder_path = None
     dataset_source_file_name = None
-    dataset2_source_folder_path = None
-    dataset2_source_file_name = None
-    
+
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
 
     def help_load(self, source_folder, source_file):
-        X = []
-        y = []
-        f = open(source_folder + source_file, 'r')
-        reader = csv.reader(f, delimiter="\t")
-        for i, line in enumerate(reader):
-            elements = [int(i) for i in line[0].split(',')]
-            # print(i, "\t", line)
-            X.append(elements[1:]) # load features into list
-            y.append(elements[0]) # load labels into list
-        f.close()
-        X = np.array(X) # change feature list into matrix
-        y = np.array(y)
-        return {'X': X, 'y': y}
+        if 1:
+            img_list = []
+            label_list = []
+            f = open(source_folder + source_file, 'rb')  # or change MNIST to other dataset names
+            data = pickle.load(f)
 
+            f.close()
+            print('training set size:', len(data['train']), 'testing set size:', len(data['test']))
+            for pair in data['train']:
+                training_image = np.asarray(pair['image'])
+                training_label = pair['label']
+                # comment out each if you want to see the outputs for yourself
+                #                 print(training_label)
+                #---------------
+                #                 plt.imshow(training_image, cmap="Greys")
+                #                 plt.show()
+                img_list.append(training_image)
+                label_list.append(training_label)
+            training_list_matrices = np.array(img_list)  # change feature list into matrix
+            training_label_matrix = np.array(label_list)
+            img_list = []
+            label_list = []
+            for pair in data['test']:
+                test_image = np.asarray(pair['image'])
+                test_label = pair['label']
+                #                 print(test_label)
+                #----------------
+                #                 plt.imshow(test_image, cmap="Greys")
+                #                 plt.show()
+                img_list.append(test_image)
+                label_list.append(test_label)
+            test_list_matrices = np.array(img_list)  # change feature list into matrix
+            test_label_matrix = np.array(label_list)
+
+            return training_list_matrices, training_label_matrix, test_list_matrices, test_label_matrix
 
     def load(self):
         print('loading data...')
-        training_data = self.help_load(self.dataset_source_folder_path, self.dataset_source_file_name)
-        testing_data = self.help_load(self.dataset2_source_folder_path, self.dataset2_source_file_name)
+        training_list_matrices, training_label_matrix, test_list_matrices, test_label_matrix = self.help_load(self.dataset_source_folder_path, self.dataset_source_file_name)
+        training_data = [training_list_matrices, training_label_matrix]
+        testing_data = [test_list_matrices, test_label_matrix]
 
         return training_data, testing_data
 
 
+# data_obj = Dataset_Loader('MNIST','')
+# data_obj.dataset_source_folder_path = '/kaggle/input/mnist/'
+# data_obj.dataset_source_file_name = 'MNIST'
+# null = data_obj.load()
+#
+# print(null[0])
